c25c1e3146ff2990bc7d6d63bd857a91e0d325ed
Shade <shadealbios@gmail.com>
2023-03-21 10:07:38 +0800

    Add activity code.

---
+++ s01/indexActivity.js
@@ -0,0 +1,21 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+let firstName = 'Shahid';
+let lastName = 'Albios';
+let age = 21;
+let hobbies = ['Basketball', 'Coding', 'Anime', 'Music', 'Studying'];
+let homeAddress = {
+    houseNumber: '101',
+    street: 'A. Cruz St.',
+    subdivision: 'Subdivision A',
+    barangay: 'Maharlika East',
+    city: 'Tagaytay',
+    province: 'Cavite',
+    zipCode: '4120'
+};
+
+console.log('First Name:', firstName);
+console.log('Last Name:', lastName);
+console.log('Age:', age);
+console.log('Hobbies:', hobbies);
+console.log('Home Address:', homeAddress);
